2ce17065e37d90a956c7e70034dc1fd945780251
Fr0styGDEV <nathanieltrotter04@gmail.com>
2023-03-20 11:47:55 -0500

    4th try

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5234/api/v1/post', {
+      const response = await fetch('https://fr0stybotai.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
